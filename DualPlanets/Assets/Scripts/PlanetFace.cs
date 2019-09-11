7929b3ab742c8d2dab4c08971e308936bf5de984
Timothy Sanderson <t.s.sanderson@hotmail.co.uk>
2019-09-11 23:38:49 +0100

    Successful sphere from angles.
    Managed to convert sphere to separate vertices from each other with rotations, instead of inflating a cube.
    http://www.dirkbertels.net/computing/greatCircles_files/great_circles_070206.pdf source of great circle intersection formulas.

--- DualPlanets/Assets/Scripts/PlanetFace.cs
+++ DualPlanets/Assets/Scripts/PlanetFace.cs
@@ -16,10 +16,10 @@ public class PlanetFace
     {
         mesh = _mesh;
         resolution = _resolution;
-        normal = _localUp;
+        normal = _localUp.normalized;
 
-        tangent = new Vector3(normal.y, normal.z, normal.x);
-        bitangent = Vector3.Cross(normal, tangent);
+        tangent = new Vector3(normal.y, normal.z, normal.x).normalized;
+        bitangent = Vector3.Cross(normal, tangent).normalized;
 
     }
 
@@ -38,8 +38,79 @@ public class PlanetFace
             for (int x = 0; x < resolution; x++)
             {
                 Vector2 percent = new Vector2(x, y) / resMin1;
-                Vector3 pointOnCube = normal + (percent.x - 0.5f) * 2.0f * tangent + (percent.y - 0.5f) * 2.0f * bitangent;
-                Vector3 pointOnSphere = pointOnCube.normalized;
+
+                Vector3 pointOnSphere;
+
+                if (percent.x == 0.5 && percent.y == 0.5)
+                {
+                    pointOnSphere = normal;
+                }
+                else if (percent.y == 0.5f)
+                {
+                    pointOnSphere = Quaternion.AngleAxis((percent.x - 0.5f) * 90.0f, tangent) * normal;
+                }
+                else if (percent.x == 0.5f)
+                {
+                    pointOnSphere = Quaternion.AngleAxis((percent.y - 0.5f) * 90.0f, bitangent) * normal;
+                }
+                else
+                {
+
+                    Vector3 rotOnTan = Quaternion.AngleAxis((percent.x - 0.5f) * 90.0f, tangent) * normal;
+                    Vector3 planeANorm = Vector3.Cross(rotOnTan, tangent).normalized;
+                    Vector3 rotOnBitan = Quaternion.AngleAxis((percent.y - 0.5f) * 90.0f, bitangent) * normal;
+                    Vector3 planeBNorm = Vector3.Cross(rotOnBitan, bitangent).normalized;
+
+                    bool swapped = false;
+                    if (planeANorm.x == 0)
+                    {
+                        if (planeBNorm.x == 0)
+                        {
+                            Debug.LogError("No x is non-zero.");
+                            return;
+                        }
+                        else
+                        {
+                            Vector3 temp = planeBNorm;
+                            planeBNorm = planeANorm;
+                            planeANorm = temp;
+                            swapped = true;
+                        }
+                    }
+                    float hDenom = planeBNorm.y * planeANorm.x - planeBNorm.x * planeANorm.y;
+                    if (hDenom == 0)
+                    {
+                        Debug.LogError("H denominator borked.");
+                        return;
+                    }
+
+                    float hValue = (planeANorm.z * planeBNorm.x - planeANorm.x * planeBNorm.z) / hDenom;
+                    float gValue = ((-planeANorm.y) * hValue - planeANorm.z) / planeANorm.x;
+
+                    float kDenom = gValue * gValue + hValue * hValue + 1;
+                    if (kDenom == 0)
+                    {
+                        Debug.LogError("Denominator of k has encountered a problem.");
+                        return;
+                    }
+                    float kValue = Mathf.Sqrt(1.0f / kDenom);
+
+                    Vector3 test1 = new Vector3(gValue * kValue, hValue * kValue, kValue);
+                    if (Vector3.Dot(test1, normal) > 0)
+                    {
+                        pointOnSphere = test1;
+                    }
+                    else
+                    {
+                        pointOnSphere = new Vector3(-gValue * kValue, -hValue * kValue, -kValue);
+                    }
+
+                    pointOnSphere = pointOnSphere.normalized;
+                }
+
+                // Vector3 pointOnCube = normal + (percent.x - 0.5f) * 2.0f * tangent + (percent.y - 0.5f) * 2.0f * bitangent;
+
+                // Vector3 pointOnSphere = pointOnCube.normalized;
                 vertices[vCount] = pointOnSphere;
                 norms[vCount] = pointOnSphere;
 
